02f49c1356674e1815a2da18b9da1b6eefdc0229
Nam Ninja <namninja@gmail.com>
2022-03-25 16:21:11 -0700

    changes App.js from class to component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+    const [ robots, setRobots ] = useState([]) //use state returns a piece of state and function that changes that state. (pass initial value of state variable, in this case empty array.)
+    const [ searchfield, setSearchfield ] = useState('')
+    const [ count, setCount ] = useState(0 )
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => this.setState({ robots: users }))
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }))
+    // }
+useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {setRobots(users)})
+       
+}, []) //add optional list to tell when to stop useEffect.  An Empty list will only tell useEffect to run once.
 
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
-
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-    render() {
-        const { robots, searchfield } = this.state;
+    
+       // const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
@@ -36,10 +44,13 @@ class App extends Component {
                 </div>
             )
         } else {
-            return (
+            return ( 
                 <div className='tc'>
                     <h1 className='f1'>RobotFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <button onClick={() => setCount(count + 1)}>Click Me!</button>
+                    <h3 className='f1'>{count}</h3>
+                    <button onClick={() => setCount(0)}>Reset</button>
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
@@ -50,7 +61,7 @@ class App extends Component {
 
             );
         }
-    }
+    
 
 }
 
